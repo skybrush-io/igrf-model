84e1779ff3c7a83983e8a85bbb1535f801c339f4
Tamas Nepusz <ntamas@gmail.com>
2022-01-03 21:04:06 +0100

    fix: fix formatting of error message when a model version is not found

--- src/igrf_model/model.py
+++ src/igrf_model/model.py
@@ -47,7 +47,7 @@ class IGRFModel:
             try:
                 fp = open_text(_DATA_PACKAGE, f"igrf{version}.txt")
             except Exception:
-                raise ValueError("unsupported model version: {version}")
+                raise ValueError(f"unsupported model version: {version}")
             with fp:
                 instance = cls.from_io(fp)
             cls._cache[version] = instance
